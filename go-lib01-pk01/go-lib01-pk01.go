21214efc2b09c6b9ea3fc751a24b2646dc3ec099
garywu125 <garywu125@gmail.com>
2018-12-02 07:51:17 +0800

    init commit

---
+++ go-lib01-pk01/go-lib01-pk01.go
@@ -0,0 +1,7 @@
+package golib01pk01
+
+import "fmt"
+
+func PrintLib01Pk01() {
+	fmt.Println("golib01/pk01")
+}
---
+++ go-lib01-pk02/go-lib01-pk02.go
@@ -0,0 +1,7 @@
+package golib01pk02
+
+import "fmt"
+
+func PrintLib01Pk02() {
+	fmt.Println("golib01/pk02")
+}
