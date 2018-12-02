28a6b41e1c49cde1273bf906fd13f67744dcb215
garywu125 <garywu125@gmail.com>
2018-12-02 08:01:35 +0800

    commit

---
+++ go-lib01-pk01/go-lib01-pk01-sub01/go-lib-pk01-sub01.go
@@ -0,0 +1,7 @@
+package golib01pk01sub01
+
+import "fmt"
+
+func PrintLib01Pk01Sub01() {
+	fmt.Println("golib01/pk01/sub01")
+}
--- go-lib01.go
+++ go-lib01.go
@@ -1,6 +1,8 @@
 package go-lib01
 
+// library package manifest
 import (
     - "github.com/garywu125/golib01/go-lib01-pk01"
+    - "github.com/garywu125/golib01/go-lib01-pk01/go-lib01-pk01-sub01"
     - "github.com/garywu125/golib01/go-lib01-pk02"
 )
